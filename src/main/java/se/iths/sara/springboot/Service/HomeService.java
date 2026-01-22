c3a49532a65f8584f9d1bf579ab9c868b46d102b
johan <johan.persson@iths.se>
2026-01-22 14:20:10 +0100

    Ändrat text i HomeService igen

--- src/main/java/se/iths/sara/springboot/Service/HomeService.java
+++ src/main/java/se/iths/sara/springboot/Service/HomeService.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class HomeService {
     public String helloFromService() {
-        return "Välkommen till home service :D";
+        return "Välkommen till service :D";
     }
 
 }
